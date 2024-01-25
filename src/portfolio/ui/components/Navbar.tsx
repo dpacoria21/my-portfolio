909bcf2a399d77953aff9e373e86598b95b11b6c
Diego Pacori <dpacoria@unsa.edu.pe>
2024-01-24 23:29:42 -0500

    Se hizo las animaciones del boton del menu y del mismo menu, pero falta coordinar el menu con el responsive

--- src/portfolio/ui/components/Navbar.tsx
+++ src/portfolio/ui/components/Navbar.tsx
@@ -1,9 +1,16 @@
 import { NavLink } from 'react-router-dom';
-import {color, motion} from 'framer-motion';
+import {AnimatePresence, motion} from 'framer-motion';
 
 import './Navbar.css';
+import { useState } from 'react';
 
 export const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const onIsOpen = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
         <nav className='navbar'>
@@ -14,13 +21,27 @@ export const Navbar = () => {
             </div>
 
             {/* menu img */}
+            <AnimatePresence mode='wait'>
+                <motion.div key={isOpen ? 'open' : 'close'} onClick={onIsOpen} className='button__toggle'>
+                    {
+                        (isOpen 
+                            ? 
+                            (
+                                <motion.svg transition={{duration: 0.3}} animate={{rotate: [-360, 0], scale: [0, 1]}} exit={{rotate: -360, scale: 0}} className='button__toggle--close' xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"><g fill="none" fillRule="evenodd"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="m12 14.122l5.303 5.303a1.5 1.5 0 0 0 2.122-2.122L14.12 12l5.304-5.303a1.5 1.5 0 1 0-2.122-2.121L12 9.879L6.697 4.576a1.5 1.5 0 1 0-2.122 2.12L9.88 12l-5.304 5.304a1.5 1.5 0 1 0 2.122 2.12z"/></g></motion.svg>
+                            )
+                            :
+                            (
+                                <motion.svg transition={{duration: 0.3}} animate={{rotate: [360, 0], scale: [0, 1]}} exit={{rotate: 360, scale: 0}} className='button__toggle--menu' xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"><path fill="currentColor" d="M3 4h18v2H3zm0 7h12v2H3zm0 7h18v2H3z"/></motion.svg>
+                            )
+                        )
+                    }
+                </motion.div>
+            </AnimatePresence>
+
             <div className='navbar--right'>
-                <div className='button__toggle'>
-                    <img className='button__toggle__img' src="" alt="" />
-                </div>
 
                 {/* menu content */}
-                <ul className='menu'>
+                <motion.ul animate={isOpen ? {opacity: 1, scale: 1} : {opacity: 0, scale: 0}} transition={{duration: 0.4}} className='menu'>
                     <li className='menu__item'>
                         <NavLink 
                             className={({isActive}) => isActive ? 'menu__item__link text__selected': 'menu__item__link'} 
@@ -66,7 +87,7 @@ export const Navbar = () => {
                             }
                         </NavLink>
                     </li>
-                </ul>
+                </motion.ul>
             </div>
         </nav>
     );
